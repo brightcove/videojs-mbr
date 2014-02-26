bb648de2eb835822e53050124840c7d3fc26dd7d
Tom Johnson <seniorflexdeveloper@gmail.com>
2014-02-26 15:15:24 -0800

    - add JSHINT to src package - updates for persistant bandwidth override

--- src/videojs-mbr.js
+++ src/videojs-mbr.js
@@ -11,98 +11,105 @@
   videojs.mbr = {
     autoSwitch: true,
     currentIndex: 0,
-    bitrateLimit: -1,
-    bandwidthOverride: -1
-  }
+    bandwidth: -1
+  };
 
   var init = function (options) {
-
     var player = this,
-        autoSwitch = (options && options.autoSwitch) ? options.autoSwitch : videojs.mbr.autoSwitch,
-        bandwidthOverride = (options && options.bandwidthOverride) ? options.bandwidthOverride : videojs.mbr.bandwidthOverride,
-        bitrateLimit = (options && options.bitrateLimit) ? options.bitrateLimit : videojs.mbr.bitrateLimit,
-        currentIndex = (options && options.currentIndex) ? options.currentIndex : videojs.mbr.currentIndex,
+    autoSwitch = (options && options.autoSwitch !== undefined) ? options.autoSwitch : videojs.mbr.autoSwitch,
+    bandwidthOverride = (options && options.bandwidthOverride !== undefined) ? options.bandwidthOverride : videojs.mbr.bandwidth,
+    currentIndex = (options && options.currentIndex !== undefined) ? options.currentIndex : videojs.mbr.currentIndex,
 
-    /*
-     Private Methods
-     */
+    originalSelectPlaylistFunction,
+    originalBandwidthFunction,
 
-        onBandwidthOverride = function () {
-          player.hls.bandwidth = bandwidthOverride;
-        };
+    onBandwidthOverride = function(value) {
+      if(value) {
+        // Don't do anything here, we are using the override value on getter.
+      } else {
+        //console.log('bandwidth overridden');
+        return bandwidthOverride;
+      }
+    },
 
-    /*
-     Public Methods
-     */
+    onManualModeSelectPlaylistOverride = function() {
+      console.log('selectPlaylist overridden due to manual mode');
+      var playlists = player.hls.master.playlists.slice();
+      return playlists[currentIndex];
+    };
 
     player.mbr.autoSwitch = function (value) {
-      if (value) {
+      if (value !== undefined) {
+        //console.log('setting autoswitch:', value);
+
         autoSwitch = value;
+
+        if(autoSwitch) {
+          player.hls.selectPlaylist = originalSelectPlaylistFunction;
+        } else {
+          player.hls.selectPlaylist = onManualModeSelectPlaylistOverride;
+        }
+
       } else {
         return autoSwitch;
       }
-    }
-
-    player.mbr.bitrateLimit = function (bitrate) {
-      if (bitrate) {
-        bitrateLimit = bitrate;
-      } else {
-        return bitrateLimit;
-      }
-    }
+    };
 
     player.mbr.bandwidthOverride = function (bandwidth) {
       if (bandwidth) {
+        //console.log('setting bandwidth override:', bandwidth);
         bandwidthOverride = bandwidth;
-        if (player.hls && player.hls.bandwidth && player.hls.sourceBuffer) {
+        if (player.hls && player.hls.sourceBuffer) {
           if (bandwidth === -1) {
-            player.hls.sourceBuffer.removeEventListener('update', onBandwidthOverride);
+            player.hls.bandwidth = originalBandwidthFunction;
           } else {
-            player.hls.sourceBuffer.addEventListener('update', onBandwidthOverride);
+            player.hls.bandwidth = onBandwidthOverride;
           }
         }
+      } else {
+        return bandwidthOverride;
       }
-
-      return bandwidthOverride;
-    }
+    };
 
     player.mbr.currentIndex = function (index) {
-      if (index) {
+      if (index !== undefined) {
+        console.log('setting current index:', index);
         if (currentIndex !== index) {
           currentIndex = index;
-          if (autoSwitch === false) {
-            player.trigger('mbr_rendition_change_request');
-          }
+
+          console.log('needs switch');
+          player.hls.media = player.hls.selectPlaylist();
+          player.currentTime(player.currentTime());
         }
+      } else {
+        return currentIndex;
       }
-      return currentIndex;
-    }
+    };
 
     /*
      Event Handlers
      */
 
-    player.on('loadedmanifest', function (event) {
+    player.on('loadedmanifest', function () {
       if (player.hls.media === undefined) {
         if (player.hls.master.playlists &&
             player.hls.master.playlists instanceof Array &&
             player.hls.master.playlists.length > 1) {
-          player.trigger('mbr_source_detected');
+
+          console.log('MBR SOURCE DETECTED BY PLUGIN');
+
+          originalBandwidthFunction = player.hls.bandwidth;
+          originalSelectPlaylistFunction = player.hls.selectPlaylist;
+
+          player.mbr.autoSwitch(autoSwitch);
+          player.mbr.currentIndex(currentIndex);
+          player.mbr.bandwidthOverride(bandwidthOverride);
+
         }
       }
-      //console.log(player.hls.master.playlists, player.hls.media);
     });
 
-    player.on('mbr_source_detected', function () {
-      console.log('MBR SOURCE DETECTED BY PLUGIN');
-    });
-
-    player.on('mbr_rendition_change_request', function () {
-      console.log('user has request a manual switch of', currentIndex, player.currentTime());
-    });
-
-
-  }
+  };
 
   videojs.plugin('mbr', function () {
     var initialize = function () {
