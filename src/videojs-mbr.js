0e29d4984eff342662b46810d1be7236903f9c77
Tom Johnson <seniorflexdeveloper@gmail.com>
2014-02-25 16:36:02 -0800

    formatting fix

--- src/videojs-mbr.js
+++ src/videojs-mbr.js
@@ -6,7 +6,7 @@
  * All rights reserved.
  */
 
-(function(window, videojs) {
+(function (window, videojs) {
 
 	videojs.mbr = {
 		autoSwitch: true,
@@ -15,7 +15,7 @@
 		bandwidthOverride: -1
 	}
 
-	var init = function(options) {
+	var init = function (options) {
 
 		var player = this,
 				autoSwitch = (options && options.autoSwitch) ? options.autoSwitch : videojs.mbr.autoSwitch,
@@ -24,18 +24,18 @@
 				currentIndex = (options && options.currentIndex) ? options.currentIndex : videojs.mbr.currentIndex,
 
 		/*
-		Private Methods
+		 Private Methods
 		 */
 
-		onBandwidthOverride = function() {
-			player.hls.bandwidth = bandwidthOverride;
-		};
+				onBandwidthOverride = function () {
+					player.hls.bandwidth = bandwidthOverride;
+				};
 
 		/*
-			Public Methods
-		*/
+		 Public Methods
+		 */
 
-		player.mbr.autoSwitch = function(value) {
+		player.mbr.autoSwitch = function (value) {
 			if (value) {
 				autoSwitch = value;
 			} else {
@@ -43,19 +43,19 @@
 			}
 		}
 
-		player.mbr.bitrateLimit = function(bitrate) {
-			if(bitrate) {
+		player.mbr.bitrateLimit = function (bitrate) {
+			if (bitrate) {
 				bitrateLimit = bitrate;
 			} else {
 				return bitrateLimit;
 			}
 		}
 
-		player.mbr.bandwidthOverride = function(bandwidth) {
-			if(bandwidth) {
+		player.mbr.bandwidthOverride = function (bandwidth) {
+			if (bandwidth) {
 				bandwidthOverride = bandwidth;
 				if (player.hls && player.hls.bandwidth && player.hls.sourceBuffer) {
-					if(bandwidth === -1) {
+					if (bandwidth === -1) {
 						player.hls.sourceBuffer.removeEventListener('update', onBandwidthOverride);
 					} else {
 						player.hls.sourceBuffer.addEventListener('update', onBandwidthOverride);
@@ -66,11 +66,11 @@
 			return bandwidthOverride;
 		}
 
-		player.mbr.currentIndex = function(index) {
-			if(index) {
-				if(currentIndex !== index) {
+		player.mbr.currentIndex = function (index) {
+			if (index) {
+				if (currentIndex !== index) {
 					currentIndex = index;
-					if(autoSwitch === false) {
+					if (autoSwitch === false) {
 						player.trigger('mbr_rendition_change_request');
 					}
 				}
@@ -79,12 +79,12 @@
 		}
 
 		/*
-			Event Handlers
+		 Event Handlers
 		 */
 
-		player.on('loadedmanifest', function(event) {
-			if(player.hls.media === undefined) {
-				if(player.hls.master.playlists &&
+		player.on('loadedmanifest', function (event) {
+			if (player.hls.media === undefined) {
+				if (player.hls.master.playlists &&
 						player.hls.master.playlists instanceof Array &&
 						player.hls.master.playlists.length > 1) {
 					player.trigger('mbr_source_detected');
@@ -93,20 +93,20 @@
 			//console.log(player.hls.master.playlists, player.hls.media);
 		});
 
-		player.on('mbr_source_detected', function() {
+		player.on('mbr_source_detected', function () {
 			console.log('MBR SOURCE DETECTED BY PLUGIN');
 		});
 
-		player.on('mbr_rendition_change_request', function() {
+		player.on('mbr_rendition_change_request', function () {
 			console.log('user has request a manual switch of', currentIndex, player.currentTime());
 		});
 
 
 	}
 
-	videojs.plugin('mbr', function() {
+	videojs.plugin('mbr', function () {
 		var initialize = function () {
-			return function() {
+			return function () {
 				this.mbr = initialize();
 				init.apply(this, arguments);
 			};
